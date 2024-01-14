106be513056e9a711cc92ca3a0d7aba15dac8364
aereal <aereal@aereal.org>
2024-01-14 22:45:33 +0900

    fix: use difference endpoint on playground

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -7,7 +7,7 @@ import "./page.css";
 
 const Page: React.FC = () => {
   const fetcher = createGraphiQLFetcher({
-    url: process.env.NEXT_PUBLIC_API_ENDPOINT ?? "",
+    url: process.env.NEXT_PUBLIC_PLAYGROUND_API_ENDPOINT ?? "",
   });
   return (
     <main className="flex min-w-max min-h-screen flex-col items-center justify-between p-24">
