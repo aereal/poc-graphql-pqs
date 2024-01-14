ae4facbc5845fd47be5d6053ec02a5924180c16f
aereal <aereal@aereal.org>
2024-01-14 22:45:13 +0900

    feat: send peristed queries

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,10 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { createPersistedQueryLink } from "@apollo/client/link/persisted-queries";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { generatePersistedQueryIdsFromManifest } from "@apollo/persisted-query-lists";
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_API_ENDPOINT ?? "",
+});
+
+const pqsLink = createPersistedQueryLink(
+  generatePersistedQueryIdsFromManifest({
+    loadManifest: () => import("../pqs-manifest.json"),
+  })
+);
 
 export const { getClient } = registerApolloClient(
   () =>
     new ApolloClient({
-      uri: process.env.NEXT_PUBLIC_API_ENDPOINT ?? "",
+      link: pqsLink.concat(httpLink),
       cache: new InMemoryCache(),
     })
 );
